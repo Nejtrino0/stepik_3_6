525bd71b2683d273de8f63b6516da1b0b3415a2a
AnatoliyKom <66serg12121255@gmail.com>
2021-04-14 10:48:18 +0300

    stepik_3_6

---
+++ test_items.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+class Test_items:
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+    def test_items_f(self, browser):
+        browser.get(Test_items.link)
+        button = WebDriverWait(browser, 5).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#add_to_basket_form > button")))
+        assert button, "Отсутствует кнопка добавления к корзину или она не активна"
+        button.click()
